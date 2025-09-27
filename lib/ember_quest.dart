7bdf4c4c4cb9b5c4b800e418353c4832e1f00acb
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-09-27 01:12:28 -0700

    build world

---
+++ lib/ember_quest.dart
@@ -0,0 +1,66 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'actors/ember.dart';
+import 'managers/segment_manager.dart';
+import '../actors/water_enemy.dart';
+import '../objects/ground_block.dart';
+import '../objects/platform_block.dart';
+import '../objects/star.dart';
+
+class EmberQuestGame extends FlameGame {
+  late EmberPlayer _ember;
+  double objectSpeed = 0.0;
+
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png',
+    ]);
+    
+    camera.viewfinder.anchor = Anchor.topLeft;
+    initializeGame();
+  }
+
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 173, 223, 247);
+  }
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+        case PlatformBlock:
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+          break;
+        case Star:
+        case WaterEnemy:
+      }
+    }
+  }
+    
+  void initializeGame() {
+    // Assume that size.x < 3200
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (640 * i).toDouble());
+    }
+
+    _ember = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+    world.add(_ember);
+  }
+}
